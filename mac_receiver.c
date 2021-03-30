8f659f1511e8097ce4ad2d57e627904fbe43d6bb
Gaëtan Fumeaux <gaetan.fumeaux@hes-so.ch>
2021-03-30 16:09:03 +0200

    Start Token code. Token not running trough MAC layer.

--- mac_receiver.c
+++ mac_receiver.c
@@ -11,13 +11,32 @@
 #include <string.h>
 #include "main.h"
 
+#include "cmsis_os2.h"
+
 
 //////////////////////////////////////////////////////////////////////////////////
 // THREAD MAC RECEIVER
 //////////////////////////////////////////////////////////////////////////////////
 void MacReceiver(void *argument)
 {
-
+	struct queueMsg_t msg;		// Message from PHY
+	osStatus_t statusMsg;			// Status of queue get
+	
+	for(;;)
+	{
+		statusMsg = osMessageQueueGet(queue_macR_id,&msg,NULL,0);		// Get the queue of macR
+		if(statusMsg == osOK)			// if the get is ok
+		{
+			if(msg.type == FROM_PHY)		// if the message is from the PHY
+			{
+				if(*(uint8_t*)msg.anyPtr == 0xFF)		// if it is a token
+				{
+					msg.type = TO_PHY;
+					osMessageQueuePut(queue_phyS_id,&msg,1,0);	// Pour l'instant renvoie token sans rien faire
+				}
+			}
+		}
+	}
 }
 
 
--- mac_sender.c
+++ mac_sender.c
@@ -12,13 +12,29 @@
 #include "main.h"
 #include "ext_led.h"
 
+#include "cmsis_os2.h"
+
 
 //////////////////////////////////////////////////////////////////////////////////
 // THREAD MAC RECEIVER
 //////////////////////////////////////////////////////////////////////////////////
 void MacSender(void *argument)
 {
-
+	struct queueMsg_t msg;		// Message from PHY
+	osStatus_t statusMsg;			// Status of queue get
+	
+	for(;;)
+	{
+		statusMsg = osMessageQueueGet(queue_macS_id,&msg,NULL,0);		// Get the queue of macR
+		if(statusMsg == osOK)			// if the get is ok
+		{
+			if(msg.type == NEW_TOKEN)		// if we want a new token
+			{
+				osMessageQueuePut(queue_phyS_id, &gTokenInterface,NULL, 0);
+				Ext_LED_PWM(4,100);	
+			}
+		}
+	}
 }
 
 
