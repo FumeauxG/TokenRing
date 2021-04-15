2961a71337ae166ffd902efb25875813c6478cc0
FumeauxG <54526511+FumeauxG@users.noreply.github.com>
2021-04-15 15:46:01 +0200

    MAC Receiver implementation

--- mac_receiver.c
+++ mac_receiver.c
@@ -42,40 +42,93 @@ void MacReceiver(void *argument)
 				{
 					//frameType = isTOKEN;
 					//i++;
+					queueMsg.type = TOKEN;
+					osMessageQueuePut(queue_macS_id, &queueMsg, NULL, 0);
 				}
 				else
 				{
-					if((qPtr[1] >> 3) == MYADDRESS) //if we are the destination
+					if((qPtr[0] >> 3) == MYADDRESS)	// if we are the source
 					{
-						length = qPtr[2];
-						//Calculate checksum
-						for(int i = 0; i<length; i++)
+						if((qPtr[3+length]&2) == 2) // if R = 1
 						{
-							checksum = (checksum + qPtr[3+i])%64;
-						}
-						
-						//MALLOC
-						msg = osMemoryPoolAlloc(memPool,osWaitForever);
-						
-						if((qPtr[3+length])>>2 == checksum)
-						{
-							
-						}
-						
-						if((qPtr[0] >> 3) == MYADDRESS)	// if we are the source
-						{
-							//DATABACK
+							if((qPtr[3+length]&1) == 1) // if A = 1
+							{
+								//DATABACK
+								queueMsg.type = DATABACK;
+								osMessageQueuePut(queue_macS_id, &queueMsg, NULL, 0);
+							}
+							else
+							{
+								qPtr[3+length] = qPtr[3+length] - 2;	// Set read and ack to 0
+								//DATA 
+								queueMsg.type = TO_PHY;
+								osMessageQueuePut(queue_phyS_id, &queueMsg, NULL, 0);
+							}
 						}
 						else
 						{
+							//MAC_ERROR_INDICATION
+							//DATABACK
+							queueMsg.type = DATABACK;
+							osMessageQueuePut(queue_macS_id, &queueMsg, NULL, 0);
+						}
+					}
+					else
+					{
+						if((qPtr[1] >> 3) == MYADDRESS) //if we are the destination
+						{
+							length = qPtr[2];
+							//Calculate checksum
+							for(int i = 0; i<length; i++)
+							{
+								checksum = (checksum + qPtr[3+i])%64;
+							}
+							
+							if((qPtr[3+length])>>2 == checksum)
+							{
+								qPtr[3+length] = qPtr[3+length] + 3;	// Set read and ack to 1
+								//DATA 
+								queueMsg.type = TO_PHY;
+								osMessageQueuePut(queue_phyS_id, &queueMsg, NULL, 0);
+								
+								//MAC_DATA_INDICATION
+								msg = osMemoryPoolAlloc(memPool,osWaitForever);	//MALLOC
+								queueMsg.type = DATA_IND;
+								memcpy(msg, &qPtr[3], qPtr[2]);		// Copy the whole message
+								msg[qPtr[2]] = '\0';	// Add \0 at the end
+								queueMsg.anyPtr = msg;
+								queueMsg.addr = (qPtr[0] >> 3);	//Source of message
+								queueMsg.sapi = qPtr[0]&7;	// Source sapi
+								
+								if(queueMsg.sapi == TIME_SAPI)
+								{
+									osMessageQueuePut(queue_timeR_id, &queueMsg, NULL, 0);
+								}
+								else if(queueMsg.sapi == CHAT_SAPI)
+								{
+									osMessageQueuePut(queue_chatR_id, &queueMsg, NULL, 0);
+								}
+							}
+							else
+							{
+								//R=1,A=0,
+								qPtr[3+length] = qPtr[3+length] + 2;	// Set read to 1 and ack to 0
+								//DATA
+								queueMsg.type = TO_PHY;
+								osMessageQueuePut(queue_phyS_id, &queueMsg, NULL, 0);
+							}
+						}
+						else
+						{
+							//DATA
 							queueMsg.type = TO_PHY;
-							osMessageQueuePut(queue_phyS_id, &queueMsg,NULL, 0);	// Send to next
-						}					
+							osMessageQueuePut(queue_phyS_id, &queueMsg, NULL, 0);
+						}
 					}
 				}
 					
-				queueMsg.type = TO_PHY;
-				osMessageQueuePut(queue_phyS_id, &queueMsg,NULL, 0);
+				//queueMsg.type = TO_PHY;
+				//osMessageQueuePut(queue_phyS_id, &queueMsg,NULL, 0);
 			}
 			
 		}
